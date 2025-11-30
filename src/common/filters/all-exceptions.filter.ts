14cb03ab68837cf684c6563d3d9d651d33606f57
Oladunmoye Adeniyi <61110110+oladunmoyeadeniyi@users.noreply.github.com>
2025-11-30 10:33:42 +0100

    Create all-exceptions.filter.ts

---
+++ src/common/filters/all-exceptions.filter.ts
@@ -0,0 +1,39 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+
+/**
+ * AllExceptionsFilter
+ * Provides consistent error responses for the entire API.
+ */
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException
+        ? exception.getResponse()
+        : exception.message || 'Internal server error';
+
+    console.error('🔥 Exception:', exception);
+
+    response.status(status).json({
+      statusCode: status,
+      path: request.url,
+      timestamp: new Date().toISOString(),
+      message,
+    });
+  }
+}
